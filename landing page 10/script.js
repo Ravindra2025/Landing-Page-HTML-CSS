188746bc0c0f85c4295dd0df21414c4b226a417f
Ravindra2025 <166825219+Ravindra2025@users.noreply.github.com>
2024-10-02 10:03:02 +0530

    Add files via upload

---
+++ landing page 10/script.js
@@ -0,0 +1,7 @@
+// Toggle button script
+const toggleBtn = document.getElementById('toggle-btn');
+const navLinks = document.getElementById('nav-links');
+
+toggleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
